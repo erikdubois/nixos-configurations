038f95c48177b98bbb3efb322c96f1fa34a4698a
Erik Dubois <erik.dubois@gmail.com>
2024-01-11 21:57:27 +0100

    update

--- picard/configuration.nix
+++ picard/configuration.nix
@@ -88,6 +88,7 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.nvidia.acceptLicense = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
--- worf/configuration.nix
+++ worf/configuration.nix
@@ -89,6 +89,7 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.nvidia.acceptLicense = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
