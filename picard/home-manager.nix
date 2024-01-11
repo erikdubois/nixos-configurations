4535757ffdd794c969c12c2870c7a823af57d038
Erik Dubois <erik.dubois@gmail.com>
2024-01-11 22:15:57 +0100

    home manager

--- picard/home-manager.nix
+++ picard/home-manager.nix
@@ -1,13 +1,13 @@
 { config, pkgs, ... }:
 let
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz home-manager";
 in
 {
   imports = [
     (import "${home-manager}/nixos")
   ];
 
-  home-manager.users.my_username = {
+  home-manager.users.erik = {
     /* The home.stateVersion option does not have a default and must be set */
     home.stateVersion = "18.09";
     /* Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ]; */
