ca4b57e76946cbefd7e11f237eba0343e89b7df7
Erik Dubois <erik.dubois@gmail.com>
2024-01-11 21:52:15 +0100

    legacy

--- worf/nvidia.nix
+++ worf/nvidia.nix
@@ -36,6 +36,6 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_390;
   };
 }
