bd519273513859836c180d60f0785ae2121988f0
Erik Dubois <erik.dubois@gmail.com>
2024-01-13 08:38:35 +0100

    firewall off

--- picard/configuration.nix
+++ picard/configuration.nix
@@ -119,12 +119,8 @@
     };
   };
 
-  # Samba
-
-  services.gvfs = {
-    enable = true;
-    #package = lib.mkForce pkgs.gnome3.gvfs;
-  };
+  # Gvfs
+  services.gvfs.enable = true;
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
@@ -133,7 +129,7 @@
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
