4f700124c26b80ba0c6100427af14898048056df
Erik Dubois <erik.dubois@gmail.com>
2024-01-13 09:11:41 +0100

    samb

--- picard/samba.nix
+++ picard/samba.nix
@@ -28,25 +28,15 @@
       map to guest = bad user
     '';
     shares = {
-      public = {
-        path = "/home/erik/DUMP";
-        browseable = "yes";
-        "read only" = "no";
-        "guest ok" = "yes";
-        "create mask" = "0644";
-        "directory mask" = "0755";
-        "force user" = "username";
-        "force group" = "groupname";
-      };
-      private = {
+      SHARED = {
         path = "/home/erik/SHARED";
         browseable = "yes";
         "read only" = "no";
-        "guest ok" = "no";
+        "guest ok" = "yes";
+        "public" = "yes";
+        "writable" = "yes";
         "create mask" = "0644";
         "directory mask" = "0755";
-        "force user" = "username";
-        "force group" = "groupname";
       };
     };
   };
