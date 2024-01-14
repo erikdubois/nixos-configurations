59d8584ad790765fb8ba4bd79aac77b993713fe4
Erik Dubois <erik.dubois@gmail.com>
2024-01-14 12:21:53 +0100

    autoupgrade

--- picard/configuration.nix
+++ picard/configuration.nix
@@ -96,6 +96,11 @@
   
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  # Auto system update
+  system.autoUpgrade = {
+      enable = true;
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
