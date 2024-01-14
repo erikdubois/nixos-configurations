6825c2796d4af7013ad52bcd97ea05ab4d57ec7d
Erik Dubois <erik.dubois@gmail.com>
2024-01-14 18:28:05 +0100

    autotiling in

--- dotfiles/.config/powermenu/powermenu.sh
+++ dotfiles/.config/powermenu/powermenu.sh
@@ -87,11 +87,7 @@ case ${chosen} in
 		run_cmd --reboot
         ;;
     $lock)
-		if [[ -x '/usr/bin/betterlockscreen' ]]; then
-			betterlockscreen -l
-		elif [[ -x '/usr/bin/i3lock' ]]; then
-			i3lock
-		fi
+		i3lock-fancy
         ;;
     $suspend)
 		run_cmd --suspend
--- riker/desktops/i3wm.nix
+++ riker/desktops/i3wm.nix
@@ -11,14 +11,13 @@
 
   services.xserver.windowManager.i3 = {
     extraPackages = with pkgs; [
-      dmenu #application launcher most people use
-      i3status # gives you the default i3 status bar
-      i3lock #default i3 screen locker
-      i3blocks #if you are planning on using i3blocks over i3status
-      betterlockscreen
-      i3lock
       #autorandr
-      #autotiling
+      autotiling
+      betterlockscreen
+      dmenu
+      i3blocks 
+      i3lock-fancy-unstable
+      i3status
     ];
   };
 }
