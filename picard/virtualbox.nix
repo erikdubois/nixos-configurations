5f0083bf2e861532a48e402963415206852b0f12
Erik Dubois <erik.dubois@gmail.com>
2024-01-14 14:25:30 +0100

    collision

--- picard/virtualbox.nix
+++ picard/virtualbox.nix
@@ -2,6 +2,6 @@
 {
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
-  virtualisation.virtualbox.guest.enable = true;
-  virtualisation.virtualbox.guest.x11 = true;
+  #virtualisation.virtualbox.guest.enable = true;
+  #virtualisation.virtualbox.guest.x11 = true;
 }
