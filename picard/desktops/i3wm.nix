9794d8ca2e4b66471c32de37779e4b96a2d1cb44
Erik Dubois <erik.dubois@gmail.com>
2024-01-14 17:59:53 +0100

    out packages

--- picard/desktops/i3wm.nix
+++ picard/desktops/i3wm.nix
@@ -15,6 +15,8 @@
       i3status # gives you the default i3 status bar
       i3lock #default i3 screen locker
       i3blocks #if you are planning on using i3blocks over i3status
+      #autorandr
+      #autotiling
     ];
   };
 }
--- riker/desktops/i3wm.nix
+++ riker/desktops/i3wm.nix
@@ -15,8 +15,8 @@
       i3status # gives you the default i3 status bar
       i3lock #default i3 screen locker
       i3blocks #if you are planning on using i3blocks over i3status
-      autorandr
-      autotiling
+      #autorandr
+      #autotiling
     ];
   };
 }
