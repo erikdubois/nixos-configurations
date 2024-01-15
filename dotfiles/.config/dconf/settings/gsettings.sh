aa2f24bf1a7d1a93df37ce042c69a513f485a94b
Erik Dubois <erik.dubois@gmail.com>
2024-01-15 13:56:37 +0100

    dconf

---
+++ dotfiles/.config/dconf/settings/gsettings.sh
@@ -0,0 +1,6 @@
+# dconf reset -f /  - to completely delete all settings
+# dconf dump / > file-full-dump
+# dconf load / > file-full-dump after changing
+
+gsettings set org.blueberry use-symbolic-icons false
+gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/arcolinux/arco-wallpaper.jpg'
