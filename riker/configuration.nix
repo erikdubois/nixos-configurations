7e775d8b5b9c397b97364024546317270b110ed4
Erik Dubois <erik.dubois@gmail.com>
2024-01-15 18:56:01 +0100

    no plasma

--- riker/configuration.nix
+++ riker/configuration.nix
@@ -55,8 +55,8 @@
   services.xserver.displayManager.startx.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  #services.xserver.displayManager.sddm.enable = true;
+  #services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
