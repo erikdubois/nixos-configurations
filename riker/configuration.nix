ffcc68648d21364988b46493ca38995e884d7bbd
Erik Dubois <erik.dubois@gmail.com>
2024-01-15 18:53:58 +0100

    startx

--- riker/configuration.nix
+++ riker/configuration.nix
@@ -50,7 +50,9 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  #services.xserver.enable = true;
+  # Disabling X11 - go for startx
+  services.xserver.displayManager.startx.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
