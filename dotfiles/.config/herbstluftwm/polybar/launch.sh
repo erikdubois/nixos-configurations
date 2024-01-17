cea10f0d14fbfd05c070e9c8bd26d086597bd787
Erik Dubois <erik.dubois@gmail.com>
2024-01-17 12:32:04 +0100

    polybar dedicated

---
+++ dotfiles/.config/herbstluftwm/polybar/launch.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# More info : https://github.com/jaagr/polybar/wiki
+
+# Install the following applications for polybar and icons in polybar if you are on ArcoLinuxD
+# awesome-terminal-fonts
+# Tip : There are other interesting fonts that provide icons like nerd-fonts-complete
+# --log=error
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+count=$(xrandr --query | grep " connected" | cut -d" " -f1 | wc -l)
+
+none+herbstluftwm|/usr/share/xsessions/herbstluftwm)
+if type "xrandr" > /dev/null; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mainbar-herbstluftwm -c ~/.config/polybar/config.ini &
+  done
+else
+polybar --reload mainbar-herbstluftwm -c ~/.config/polybar/config.ini &
+fi
