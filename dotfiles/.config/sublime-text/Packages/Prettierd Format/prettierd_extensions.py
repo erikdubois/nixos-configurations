6704052bf31ea2c6c24f03222e138a229af560cb
Erik Dubois <erik.dubois@gmail.com>
2024-01-17 08:59:06 +0100

    new sublime-text settings

--- dotfiles/.config/sublime-text/Packages/Prettierd Format/prettierd_extensions.py
+++
@@ -1,5 +0,0 @@
-valid_extensions = [
-    "cjs", "component.html", "css", "cts", "flow", "gql", "graphql", "hbs", "handlebars", 
-    "html", "js", "json", "json5", "jsx", "less", "markdown", "md", "mdx", "mjs", "mts", 
-    "pug", "scss", "ts", "tsx", "vue", "yaml", "yml"
-]
--- dotfiles/.config/sublime-text/Packages/Prettierd Format/prettierd_save_without_format.py
+++
@@ -1,7 +0,0 @@
-import sublime
-import sublime_plugin
-
-class PrettierdSaveWithoutFormatCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        self.view.settings().set("skip_formatting", True)
-        self.view.run_command("save")
