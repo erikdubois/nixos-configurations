52ad7a637a3554e8e73e838eee31e3cdf39cf919
Erik Dubois <erikdubois@users.noreply.github.com>
2024-01-18 15:43:53 +0100

    Update configuration.nix

--- riker/configuration.nix
+++ riker/configuration.nix
@@ -68,6 +68,8 @@
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+  # for wayland dark theme  
+  #programs.dconf.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
