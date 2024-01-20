0369deff2154676cfae2a1afd8ab520d2fad6a72
Erik Dubois <erik.dubois@gmail.com>
2024-01-20 07:06:29 +0100

    chadwm

--- riker/desktops/chadwm.nix
+++ riker/desktops/chadwm.nix
@@ -2,36 +2,36 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
-
-{
-  #services.xserver.displayManager.defaultSession = "none+dwm";
-  services.xserver.windowManager.dwm.enable = true;
-  nixpkgs.overlays = [
-    (final: prev: {
-      dwm = prev.dwm.overrideAttrs (old: { src = /home/erik/.config/arco-chadwm/chadwm ;});
-    })
-  ];
-}
+# { config, pkgs, ... }:
 
 # {
 #   #services.xserver.displayManager.defaultSession = "none+dwm";
 #   services.xserver.windowManager.dwm.enable = true;
-
-#   pkgs.stdenv.mkDerivation = {
-#     # name of our derivation
-#     name = "chadwm";
-
-#     # sources that will be used for our derivation.
-#     src = ./config/arco-chadwm/chadwm;
-
-#     buildInputs = [Imlib2 libX11 libXft libXinerama gnumake acpi];
-
-#     installPhase = ''
-#       mkdir $out
-#       rm config.h -rf
-#       make dwm
-#       mv dwm $out
-#     '';
-#   };
+#   nixpkgs.overlays = [
+#     (final: prev: {
+#       dwm = prev.dwm.overrideAttrs (old: { src = /home/erik/.config/arco-chadwm/chadwm ;});
+#     })
+#   ];
 # }
+
+{
+  #services.xserver.displayManager.defaultSession = "none+dwm";
+  services.xserver.windowManager.dwm.enable = true;
+
+  pkgs.stdenv.mkDerivation = {
+    # name of our derivation
+    name = "dwm";
+
+    # sources that will be used for our derivation.
+    src = ./config/arco-chadwm/chadwm;
+
+    buildInputs = [Imlib2 libX11 libXft libXinerama gnumake acpi];
+
+    installPhase = ''
+      mkdir $out
+      rm config.h -rf
+      make dwm
+      mv dwm $out
+    '';
+  };
+}
