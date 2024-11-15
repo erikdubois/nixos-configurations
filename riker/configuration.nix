a73f02d075c458af7d62f4eb0d96e844b1681619
arcolinuxz <arcolinuxinfo@gmail.com>
2024-11-15 08:52:59 +0100

    firefox out

--- riker/configuration.nix
+++ riker/configuration.nix
@@ -142,9 +142,6 @@
     ];
   };
 
-  #install firefox
-  programs.firefox.enable = true;
-
   # Enable automatic login for the user.
   services.displayManager.autoLogin.enable = true;
   services.displayManager.autoLogin.user = "erik";
