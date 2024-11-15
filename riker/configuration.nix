1b7599f6b1e0945db5042571997652c244f5e19c
arcolinuxz <arcolinuxinfo@gmail.com>
2024-11-15 08:51:36 +0100

    plasma6

--- riker/configuration.nix
+++ riker/configuration.nix
@@ -66,7 +66,8 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
   # for wayland dark theme  
   #programs.dconf.enable = true;
 
@@ -140,6 +141,9 @@
       firefox
     ];
   };
+
+  #install firefox
+  programs.firefox.enable = true;
 
   # Enable automatic login for the user.
   services.displayManager.autoLogin.enable = true;
